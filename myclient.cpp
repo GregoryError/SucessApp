2ae55365fa7c8dd3576593aaebb86626888ff230
Gregory Kolesnikov <32989301+GregoryError@users.noreply.github.com>
2018-06-09 06:56:37 +0300

    Add files via upload

--- myclient.cpp
+++ myclient.cpp
@@ -106,7 +106,7 @@ void MyClient::slotReadyRead()
             pay_day += c;
         }
 
-        if(space == 4 && c != ' ')
+        if(space >= 4)
         {
             paket += c;
         }
@@ -183,7 +183,7 @@ void MyClient::Sender(const QString &msg)
 void MyClient::connectToHost()
 {
   //m_pTcpSocket->connectToHostEncrypted("176.125.152.88", 2323);
-    m_pTcpSocket->connectToHostEncrypted("192.168.7.128", 4242);
+    m_pTcpSocket->connectToHostEncrypted("10.4.43.99", 4242);
 
 }
 
