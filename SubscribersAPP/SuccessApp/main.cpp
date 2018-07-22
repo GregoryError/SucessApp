47c7487cc50bf8e4b83cad61d4a6b07995840129
Gregory <kolesnikoff2085@yandex.ru>
2018-07-22 12:57:58 +0300

    some changes in MAIN

--- SubscribersAPP/SuccessApp/main.cpp
+++ SubscribersAPP/SuccessApp/main.cpp
@@ -1,11 +1,23 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include "myclient.h"
+#include "backend.h"
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    app.setApplicationDisplayName("Успех");
+
+    app.setOrganizationName("Success");
+    app.setOrganizationDomain("www.comfort-tv.ru/");
+    app.setApplicationName("Success");
+
+
+
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
